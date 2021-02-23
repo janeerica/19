0676d94a9e9581a094403433dcf0750c4688102b
janeerica <76727311+janeerica@users.noreply.github.com>
2021-02-23 12:36:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("my first console");
+var first="jane";
+var last="cardozo";
+console.log(first);
+var full=first.concat(last);
+console.log(full);
+var full1=first+" "+last;
+console.log(full1);
+var len=full1.length;
+console.log(len);
+var replace=full1.replace("cardozo","erica");
+console.log(replace);
+console.log(replace.toUpperCase());
+
+var sqrt=Math.sqrt(16);
+var max=Math.max(15,10);
+var min=Math.min(15,10);
+var pow=Math.pow(4,3);
+console.log(sqrt);
+console.log(max);
+console.log(pow);
+console.log(min);
